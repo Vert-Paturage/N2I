bdd86094fef97a1647858b332186692bbe73c2b7
Romain <romain.reydel@gmail.com>
2024-12-06 01:24:23 +0100

    ca smell toujours ici

--- MyAPI2_vFinal/appli.cs
+++ MyAPI2_vFinal/appli.cs
@@ -3,9 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 var app = builder.Build();
 
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions{
+    OnPrepareResponse = ctx =>
+    {
+        // Restreindre les fichiers accessibles à certaines extensions
+        var allowedExtensions = new[] { ".html", ".js", ".css", ".png", ".glb" };
+        var ext = Path.GetExtension(ctx.File.Name).ToLowerInvariant();
+        if (!allowedExtensions.Contains(ext))
+        {
+            ctx.Context.Response.StatusCode = 403; // Refuser l'accès
+            ctx.Context.Response.Body = Stream.Null;
+        }
+    }
+});
 
 app.MapGet("/", () => Results.Redirect("./index.html"));
 
